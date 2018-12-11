543ee66239c2a52b32cd1db0b165db5333781c63
ikimono524@gmail.com <ht235711@yahoo.co.jp>
2018-12-11 14:41:24 +0900

    ファイル移動

--- ImagePropability.py
+++ ImagePropability.py
@@ -26,14 +26,13 @@ np.set_printoptions(precision=10, suppress=True, threshold=np.inf, linewidth=100
 fileName = "water-lily-3784022__340"
 inputImgPath = "outimg/continuity_hue/All/" + fileName
 
-intercept = np.load("intercept.npy")
-coef = np.load("coef.npy") #多重配列になっていたから先頭だけ抽出
+intercept = np.load("LogisticRegresionResult/intercept.npy")
+coef = np.load("LogisticRegresionResult/coef.npy") #多重配列になっていたから先頭だけ抽出
 
 imageFeature = ImageFeature.ImageFeature()
 propability = np.empty(0)
 
 for it in range(100):
-    #Pathに日本語が含まれるとエラー
     inputImg = cv2.imread(inputImgPath + "_" + str(it) + ".jpg", cv2.IMREAD_COLOR)
 
     #正規化と整形
